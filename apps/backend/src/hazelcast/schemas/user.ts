372061b82f71a0b0ad23050c359cc155869c7eb7
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-08-19 01:33:07 +0200

    hazelcast integration 2

--- apps/backend/src/hazelcast/schemas/user.ts
+++ apps/backend/src/hazelcast/schemas/user.ts
@@ -1,15 +1,18 @@
-import { User } from "../../db/schema/users";
+import { db } from "../../db";
+import { User, users } from "../../db/schema/users";
 import { hazelClient } from "../connect";
 
-export type CachedUser = Pick<User, "id" | "avatar" | "banner" | "bio" | "embedding" | "followerCount" | "followingCount" | "handle" | "name">
+export type CachedUser = Pick<User, "id" | "avatar" | "banner" | "bio" | "followerCount" | "followingCount" | "handle" | "name" | "createdAt" | "embedding">
+
+export type CachedUserSerialized = Pick<User, "id" | "avatar" | "banner" | "bio" | "followerCount" | "followingCount" | "handle" | "name"> & { createdAt: number, embedding: string | null }
 
 await hazelClient.getSql().execute(`
 CREATE OR REPLACE MAPPING users (
     __key VARCHAR,             
     name VARCHAR,
-    createdAt TIMESTAMP,
+    createdAt BIGINT,
     profilePicture JSON,
-    embedding JSON
+    embedding VARCHAR
 )
 TYPE IMap
 OPTIONS (
@@ -18,12 +21,39 @@ OPTIONS (
 )
 `);
 
+const usersMap = await hazelClient.getMap<string, CachedUserSerialized>('users');
+
 // 3. Insert data
-const usersMap = await hazelClient.getMap<string, CachedUser>('users');
-await usersMap.set("id", { name: 'name', id: "id", handle: "handle", embedding: null, followerCount: 0, followingCount: 0, avatar: null, banner: null, bio: null });
+const testUsers = await db.select().from(users).limit(10)
+
+const serialized = testUsers.map(serializeUser)
+await usersMap.putAll(serialized.map(u => [u.id, u]))
 
 // 4. Query
 const result = await hazelClient.getSql().execute('SELECT * FROM users');
 for await (const row of result) {
     console.log(row);
+}
+
+export function serializeUser({ createdAt, embedding, id, name, handle, bio, avatar, banner, followerCount, followingCount }: CachedUser): CachedUserSerialized {
+    return {
+        id,
+        name,
+        handle,
+        bio,
+        avatar,
+        banner,
+        followerCount,
+        followingCount,
+        createdAt: new Date(createdAt).getTime(),
+        embedding: JSON.stringify(embedding)
+    }
+}
+
+export function deserializeUser({ createdAt, embedding, ...rest }: CachedUserSerialized): CachedUser {
+    return {
+        ...rest,
+        createdAt: new Date(createdAt),
+        embedding: embedding ? JSON.parse(embedding) : null
+    }
 }
